df586fc5c026742e2bb75b3cbcd09467e547ac74
nestorsan77 <nestorsan77@gmail.com>
2024-11-22 11:28:03 +0100

    cambios de clima realizados

--- app/models/VictronEnergy.php
+++ app/models/VictronEnergy.php
@@ -3,13 +3,15 @@ class VictronEnergy{
     private $url;
     private $api_key;
     private $id_access_token;
+    private $id_installation;
 
     //definimos el constructor de la clase
     public function __construct()
     {
-        $this->url = 'https://monitoringapi.solaredge.com/';
-        $this->api_key = '61ffd7dc0f459c485f527f1056d9492e6418e58adf74cd34be9ce768ac6fe576';
+        $this->url = 'https://vrmapi.victronenergy.com/v2/';
+        $this->api_key = 'Token 61ffd7dc0f459c485f527f1056d9492e6418e58adf74cd34be9ce768ac6fe576';
         $this->id_access_token = 2160468;
+        $this->id_installation = 58178;
     }
 
     //definimos el getter y setter
@@ -30,6 +32,12 @@ class VictronEnergy{
     }
     public function setIdAccessToken($id_access_token){
         $this->id_access_token = $id_access_token;
+    }
+    public function getIdInstallation(){
+        return $this->id_installation;
+    }
+    public function setIdInstallation($id_installation){
+        $this->id_installation = $id_installation;
     }
     //Documentacion API https://vrm-api-docs.victronenergy.com/#/
 }
